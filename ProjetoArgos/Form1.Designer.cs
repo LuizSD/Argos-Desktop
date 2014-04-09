b500a99609428b2cf7dfafe4e9bfe03eb1d339b7
LuizSD <lsantosdutra@gmail.com>
2014-04-09 00:37:10 -0300

    Acrescendo botões e funcionalidades

--- ProjetoArgos/Form1.Designer.cs
+++ ProjetoArgos/Form1.Designer.cs
@@ -29,35 +29,99 @@
         private void InitializeComponent()
         {
             this.Lampada1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Lampada2 = new System.Windows.Forms.Button();
+            this.Lampada3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Lampada1
             // 
-            this.Lampada1.Location = new System.Drawing.Point(12, 12);
+            this.Lampada1.Location = new System.Drawing.Point(12, 25);
             this.Lampada1.Name = "Lampada1";
             this.Lampada1.Size = new System.Drawing.Size(75, 23);
             this.Lampada1.TabIndex = 0;
-            this.Lampada1.Text = "Liga";
+            this.Lampada1.Text = "Ligar";
             this.Lampada1.UseVisualStyleBackColor = true;
-            this.Lampada1.Click += new System.EventHandler(this.button1_Click);
+            this.Lampada1.Click += new System.EventHandler(this.Lampada1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lâmpada 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Lâmpada 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Lâmpada 3";
+            // 
+            // Lampada2
+            // 
+            this.Lampada2.Location = new System.Drawing.Point(12, 78);
+            this.Lampada2.Name = "Lampada2";
+            this.Lampada2.Size = new System.Drawing.Size(75, 23);
+            this.Lampada2.TabIndex = 4;
+            this.Lampada2.Text = "Ligar";
+            this.Lampada2.UseVisualStyleBackColor = true;
+            this.Lampada2.Click += new System.EventHandler(this.Lampada2_Click);
+            // 
+            // Lampada3
+            // 
+            this.Lampada3.Location = new System.Drawing.Point(12, 130);
+            this.Lampada3.Name = "Lampada3";
+            this.Lampada3.Size = new System.Drawing.Size(75, 23);
+            this.Lampada3.TabIndex = 5;
+            this.Lampada3.Text = "Ligar";
+            this.Lampada3.UseVisualStyleBackColor = true;
+            this.Lampada3.Click += new System.EventHandler(this.Lampada3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Lampada3);
+            this.Controls.Add(this.Lampada2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Lampada1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
             this.Text = "Argos";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button Lampada1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Lampada2;
+        private System.Windows.Forms.Button Lampada3;
     }
 }
 
--- ProjetoArgos/Form1.cs
+++ ProjetoArgos/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ProjetoArgos.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +17,26 @@ namespace ProjetoArgos
         public Form1()
         {
             InitializeComponent();
-            if()
+            //Inicializa o Arduino
+            var porta = new SerialPort("COM5", 9600);
+            var controle = new ControleEstado(porta);
+
+            //Verifica os Estados
+            var primeiro = new Dispositivo("1");
+            var estadoPrimeira = controle.VerificaEstado(primeiro);
+
+            var segundo = new Dispositivo("2");
+            var estadoSegunda = controle.VerificaEstado(primeiro);
+
+            var terceiro = new Dispositivo("3");
+            var estadoTerceira = controle.VerificaEstado(primeiro);
+
+            Lampada1.Text = estadoPrimeira.ToString();
+            Lampada2.Text = estadoSegunda.ToString();
+            Lampada3.Text = estadoTerceira.ToString(); 
+
+           
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -23,11 +44,69 @@ namespace ProjetoArgos
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Lampada1_Click(object sender, EventArgs e)
         {
+            var porta = new SerialPort("COM5", 9600);
+            var controle = new ControleEstado(porta);
+
+            var primeiro = new Dispositivo("1");
+            var estadoLampada = controle.VerificaEstado(primeiro);
+
+            if (estadoLampada.ToString() == Lampada1.Text) {
+                var estadoNovo = estadoLampada.Equals(Estado.Ligado) ? Estado.Desligado : Estado.Ligado;
+                var resultado = controle.AtualizarStatus(primeiro, estadoNovo);
+                Lampada1.Text = estadoNovo.ToString();
+            }
+            else
+            {
+                MessageBox.Show("A Lâmpada já está no estado " + estadoLampada);
+            }
             
-            
-            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lampada2_Click(object sender, EventArgs e)
+        {
+            var porta = new SerialPort("COM5", 9600);
+            var controle = new ControleEstado(porta);
+
+            var primeiro = new Dispositivo("2");
+            var estadoLampada = controle.VerificaEstado(primeiro);
+
+            if (estadoLampada.ToString() == Lampada1.Text)
+            {
+                var estadoNovo = estadoLampada.Equals(Estado.Ligado) ? Estado.Desligado : Estado.Ligado;
+                var resultado = controle.AtualizarStatus(primeiro, estadoNovo);
+                Lampada1.Text = estadoNovo.ToString();
+            }
+            else
+            {
+                MessageBox.Show("A Lâmpada já está no estado " + estadoLampada);
+            }
+        }
+
+        private void Lampada3_Click(object sender, EventArgs e)
+        {
+            var porta = new SerialPort("COM5", 9600);
+            var controle = new ControleEstado(porta);
+
+            var primeiro = new Dispositivo("3");
+            var estadoLampada = controle.VerificaEstado(primeiro);
+
+            if (estadoLampada.ToString() == Lampada1.Text)
+            {
+                var estadoNovo = estadoLampada.Equals(Estado.Ligado) ? Estado.Desligado : Estado.Ligado;
+                var resultado = controle.AtualizarStatus(primeiro, estadoNovo);
+                Lampada1.Text = estadoNovo.ToString();
+            }
+            else
+            {
+                MessageBox.Show("A Lâmpada já está no estado " + estadoLampada);
+            }
         }
     }
 }
