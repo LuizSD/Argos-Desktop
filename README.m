1afcfe753f159795332a13e45cf626132a6e506a
Luiz Felipe dos Santos Dutra <lsantosdutra@gmail.com>
2014-11-23 14:10:05 -0200

    Create README.m

---
+++ README.m
@@ -0,0 +1,5 @@
+Argos-Desktop
+=============
+
+Argos é um sistema de gerenciamento Residencial, capaz de tornar sua casa mais interativa. 
+Deixando você no controle com Apenas um toque. Argos DeskTop é sua versão para computadores.
